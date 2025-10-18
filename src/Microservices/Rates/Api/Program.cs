000944bc9efdcee684eb5517ba30a0200450d990
Rawenvoys <lukwis95@gmail.com>
2025-10-18 23:48:53 +0200

    Test Nbp Api client

--- src/Microservices/Rates/Api/Program.cs
+++ src/Microservices/Rates/Api/Program.cs
@@ -1,9 +1,14 @@
+using CurrencyRates.Clients.Nbp.Client;
+using CurrencyRates.Clients.Nbp.Client.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Logging.ClearProviders();
 builder.Logging.AddConsole();
 
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddNbpApiClient(builder.Configuration);
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
@@ -17,9 +22,13 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapGet("/", () =>
+app.MapGet("/", async ([FromServices] INbpApi _nbpApi, [FromServices] ILogger _logger) =>
 {
-
+    _logger.LogDebug("Start sending request");
+    var response = await _nbpApi.Get("B", "2002-01-02", "2002-03-31");
+    _logger.LogDebug("Response: {tablesCount}", response.Count);
+    foreach(var table in response)
+        _logger.LogInformation("Table number: {no}", table.No);
 })
 .WithName("Get current rates")
 .WithOpenApi();
